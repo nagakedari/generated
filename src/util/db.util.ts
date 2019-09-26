885186e5f27ee13e32923e3f4b9367ca88d26018
nagakedari <nagadurga.kedari@gmail.com>
2019-09-25 23:56:36 -0500

    Added some log statements

--- src/util/db.util.ts
+++ src/util/db.util.ts
@@ -5,9 +5,12 @@ import * as AWS from 'aws-sdk';
 class DBUtil {
     @logMethod
     static async connectToDatabase(cachedDb) {
+        console.log('****************************log:',14);
         if (cachedDb && cachedDb.serverConfig.isConnected()) {
+            console.log('****************************log:',15);
             return Promise.resolve(cachedDb);
         }
+        console.log('****************************log:',16);
         let password = this.getMongoDbPasswordFromParameterStore('mongo_password');
         console.log('username Parameter from store ********* ',process.env.MongoUser);
         let dbUrl = 'mongodb+srv://' + process.env.MongoUser + ':' + password + '@' + 
