fffe8164194652a8ec53abdc8bb36e61c56516a8
nagakedari <nagadurga.kedari@gmail.com>
2019-09-27 23:23:41 -0500

    testing log statements in cloud watch

--- src/util/db.util.ts
+++ src/util/db.util.ts
@@ -11,7 +11,7 @@ class DBUtil {
             return Promise.resolve(cachedDb);
         }
         console.log('****************************log:',16);
-        let password = this.getMongoDbPasswordFromParameterStore('mongo_password');
+        let password = await this.getMongoDbPasswordFromParameterStore('mongo_password');
         console.log('username Parameter from store ********* ',process.env.MongoUser);
         let dbUrls = 'mongodb+srv://' + process.env.MongoUser + ':' + password + '@' + 
                     process.env.MongoCluster + '/' + process.env.MongoDBName;
