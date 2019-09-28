cc4b69eaf5c08b715869421dba9d974e11ecf0dd
nagakedari <nagadurga.kedari@gmail.com>
2019-09-28 08:42:08 -0500

    testing log statements in cloud watch

--- src/util/db.util.ts
+++ src/util/db.util.ts
@@ -5,17 +5,17 @@ import * as AWS from 'aws-sdk';
 class DBUtil {
     // @logMethod
     static async connectToDatabase(cachedDb) {
-        console.log('****************************log:',14);
+        console.log('****************************log:', 14);
         if (cachedDb && cachedDb.serverConfig.isConnected()) {
-            console.log('****************************log:',15);
+            console.log('****************************log:', 15);
             return Promise.resolve(cachedDb);
         }
-        console.log('****************************log:',16);
+        console.log('****************************log:', 16);
         let password = await this.getMongoDbPasswordFromParameterStore('mongo_password');
-        console.log('username Parameter from store ********* ',process.env.MongoUser);
-        let dbUrls = 'mongodb+srv://' + process.env.MongoUser + ':' + password + '@' + 
-                    process.env.MongoCluster + '/' + process.env.MongoDBName;
-        console.log('URLLLLLLLLLL...........................:',dbUrls)
+        console.log('username Parameter from store ********* ', process.env.MongoUser);
+        let dbUrls = 'mongodb+srv://' + process.env.MongoUser + ':' + password + '@' +
+            process.env.MongoCluster + '/' + process.env.MongoDBName;
+        console.log('URLLLLLLLLLL...........................:', dbUrls)
 
         let dbUrl = 'mongodb+srv://mongo_user:wAAbUC36OzKcm0hN@cluster0-knpmq.mongodb.net/boilerplate';
         cachedDb = await mongoose.connect(dbUrl, { useNewUrlParser: true });
@@ -23,22 +23,32 @@ class DBUtil {
     }
 
     // @logMethod
-    static async getMongoDbPasswordFromParameterStore(parameterName: string): Promise<any> {
-        let parameterResponse;
-       try {
-        let ssmAgent = new AWS.SSM();
-        let params = {
-            Name: parameterName, 
-            WithDecryption: true
-          };
-        console.log('Before Invoking Parameter from store ********* ');
-        parameterResponse = await ssmAgent.getParameter(params);
-        console.log('Password Parameter from store ********* ',parameterResponse);
-        
-       } catch (e){
-           console.log('Exception while invoking the parameter store', e);
-       }
-       return Promise.resolve(parameterResponse);
+    static getMongoDbPasswordFromParameterStore(parameterName: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            let parameterResponse;
+            try {
+                let ssmAgent = new AWS.SSM();
+                let params = {
+                    Name: parameterName,
+                    WithDecryption: true
+                };
+                console.log('Before Invoking Parameter from store ********* ');
+                ssmAgent.getParameter(params, function (err, data) {
+                    if (err) {
+                        console.log('errrrrrrrrrrrrrrr******* ', err);
+                        reject(err);
+                    } else {
+                        resolve(data);
+                        console.log('Password Parameter from store ********* ', data);
+                    }
+
+                });
+
+            } catch (e) {
+                console.log('Exception while invoking the parameter store', e);
+            }
+        });
+
     }
 }
 
